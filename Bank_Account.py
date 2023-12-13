35ebccfc4a44220eb35fc0667142ffc0faee7c90
Athulya-P-03 <147337867+Athulya-P-03@users.noreply.github.com>
2023-12-13 12:15:39 +0530

    Create Bank_Account.py

---
+++ Bank_Account.py
@@ -0,0 +1,12 @@
+class Bank:
+    def_init_(self):
+        self.balance=0
+        print("_Create an Account_")
+        self.name=input("Enter the name : ")
+        self.acctno=int(input("Enter the Account number : "))
+        self.typeofac=input("Enter the type of account : ")
+    def deposit(self):
+        amount=int(input("Enter the amount to be deposited : "))
+        
+        
+    
