feaac9f494cd0f5c760030b97f4a082406de8ca3
Athulya-P-03 <147337867+Athulya-P-03@users.noreply.github.com>
2023-12-14 23:00:19 +0530

    sqrt_factorial_pi.py

---
+++ sqrt_factorial_pi.py
@@ -0,0 +1,5 @@
+import math
+number = int(input("Enter a number: "))
+print("Square root of",number,":",math.sqrt(number))
+print("Factorial of",number,":",math.factorial(number))
+print("Value of pi:",math.pi)
