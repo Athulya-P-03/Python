821f79f39a7242da6eb9350c141eb99d5e877a9a
Athulya-P-03 <147337867+Athulya-P-03@users.noreply.github.com>
2023-12-14 23:29:03 +0530

    Update raise_program.py

--- raise_program.py
+++ raise_program.py
@@ -1,6 +1,6 @@
 try:
     num = int(input("Enter a positive integer:"))
     if(num <= 0):
-         raise ValueError("That is a negative number!")
+         raise ValueError("This is a negative number!")
 except ValueError as e:
     print(e)
