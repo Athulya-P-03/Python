9523627ee2a3a3e118bc3fe98672ca894f7e5a3c
Athulya-P-03 <147337867+Athulya-P-03@users.noreply.github.com>
2023-12-31 10:50:19 +0530

    Update shuffle_element_list.py

--- shuffle_element_list.py
+++ shuffle_element_list.py
@@ -1,7 +1,5 @@
 import random
-
-original_list = ["a",5,8,"b","c",9,"d"]
-shuffled_list = random.sample(original_list, len(original_list))
-
-print("Original list:", original_list)
-print("Shuffled list:", shuffled_list)
+original_list=[1,"apple",3.14,"banana",42,"orange"]
+shuffled_list=random.sample(original_list,len(original_list))
+print("original list:",original_list)
+print("shuffled list:",shuffled_list)
